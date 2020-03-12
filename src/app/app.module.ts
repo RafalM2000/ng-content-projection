478baa2a0f06a53b582bbdb3f7676046b430a396
Rafal <rmf@europe.com>
2020-03-12 06:50:04 +0100

    burger component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BurgerComponent } from './feature/burger/burger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerComponent
   ],
   imports: [
     BrowserModule
