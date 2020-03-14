21d655ecc7458c92ac3397676998494a2b41e9c8
Rafal <rmf@europe.com>
2020-03-14 06:29:43 +0100

    basic logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title1: string = 'Angular-In-Examples';
   title2: string = 'ng-content';
+
+  public temp: number;
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      this.temp = Math.round(Math.random() * 10);
+     }, 1000);
+  }
 }
---
+++ src/app/feature/lusowo/lusowo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lusowo',
+  templateUrl: './lusowo.component.html',
+  styleUrls: ['./lusowo.component.css']
+})
+export class LusowoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
